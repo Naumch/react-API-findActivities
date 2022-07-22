7e674d83c0cffe08c804b704c93dca224f3195b6
Anna <anya.naumova.1994@gmail.com>
2022-07-22 16:49:10 +0300

    removed the extra filter from the component with participants

--- src/SearchingByParticipants.jsx
+++ src/SearchingByParticipants.jsx
@@ -4,17 +4,12 @@ import Activity from "./Activity";
 
 function SearchingByParticipants() {
   const [data, setData] = useState([]);
-  const [value, setValue] = useState(0);
-
-  const select = [2, 3];
-  const options = select.map((text, index) => {
-		return <option key={index} value={index}>{text}</option>;
-	});
+  //const [filter, setFilter] = useState(null);
 
   function findByParticipants() {
     const fetchData = async () => {
       const result = await axios(
-        `http://www.boredapi.com/api/activity?participants=${select[value]}`,
+        `http://www.boredapi.com/api/activity?participants=${getRandom(2, 5)}`,
       );
 
       setData(result.data);
@@ -22,13 +17,16 @@ function SearchingByParticipants() {
 
     fetchData();
   }
+
+  function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
   
   return (
     <div>
       Найди занятие для компании
-      <select value={value} onChange={event => setValue(event.target.value)}>
-        {options}
-      </select>
       <button onClick={findByParticipants}>Найти</button>
       <Activity data={data}/>
     </div>
